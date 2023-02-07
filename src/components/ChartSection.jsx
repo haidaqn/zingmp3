908dcaf8113ac4d7582852fff0062e7752dcff78
haidang <haidang02032003@gmail.com>
2023-02-08 00:37:50 +0700

    error line react chartjs

--- src/components/ChartSection.jsx
+++ src/components/ChartSection.jsx
@@ -1,34 +1,59 @@
 /* eslint-disable no-unused-vars */
 import React, { memo ,useState, useEffect} from 'react';
 import imgChart from '../assets/chart.jpg';
-
 import { useSelector } from 'react-redux';
-import { Chart as ChartJS } from 'chart.js/auto';
+import { Chart as ChartJS, registerables  } from 'chart.js/auto';
 import { Line } from 'react-chartjs-2';
+
+ChartJS.register(...registerables);
 
 const ChartSection = () => {
 
-  const options = {
-    responsive: true,
-    pointRadius: 0,
-    aspectRatio: 4,
-    scales: {
-      y: {
-        ticks: { display: false },
-        grid: {borderDash : [1,4], color : 'gray'}
-      },
-      x: {
-        ticks: { color: 'blue' },
-        grid: {color : 'transparent'}
-      }
-    },
-    plugins: {
-      legend : false 
-    }
-  }
+  
   const { chart } = useSelector(state => state.app);
   const [data, setData] = useState({});
+/* 
+  const encodeId = [];
+  for (let i = 0; i < 3; i++) { 
+    encodeId.push(chart?.chart?.items[Object.values(chart?.chart?.items)[i]]);
+  }
+  // console.log(encodeId);
 
+  let handleTimes = (times) => {
+        let labels = [];
+        times.forEach((item, index) => {
+            if (index % 2 === 0 && index < 23) {
+                labels.push(item.hour + ':00');
+            }
+        });
+        return labels;
+  };
+  
+  let handleCounter = (items, encodeId) => {
+        let counters = [];
+        items[encodeId].forEach((item, index) => {
+            if (index % 2 === 0 && index < 24) {
+                counters.push(item.counter);
+            }
+        });
+        return counters;
+  }; */
+  const options = {
+      responsive: true,
+      pointRadius: 0,
+      aspectRatio: 4,
+      scales: {
+        y: {
+          ticks: { display: false },
+          grid: { borderDash: [4, 20], color: 'white' }
+        },
+        x: {
+          ticks: { color: 'blue' },
+          grid: { color: 'transparent' }
+        }
+      },
+      plugins: {legend : false}
+    } 
   useEffect(()=>{
     const labels = chart?.chart?.times?.filter(item => +item.hour % 2 === 0)?.map(item => item.hour);
     const datatset = [];
@@ -37,16 +62,22 @@ const ChartSection = () => {
         datatset.push({
           data: chart?.chart?.items[Object.keys(chart?.chart?.items)[i]]?.filter(item => +item.hour % 2 === 0)?.map(item => item.counter),
           borderColor: i === 0 ? 'blue' : i === 1 ? 'yellow' : 'red',
-          tension: 0.2,
+          backgroundColor: i === 0 ? 'rgb(74,144,226)' : i === 1 ? 'rgb(39, 189, 156)' : 'rgb(227, 80, 80)',
+          pointBorderColor: i === 0 ? 'rgb(74,144,226)' : i === 1 ? 'rgb(39, 189, 156)' : 'rgb(227, 80, 80)',
+          pointHoverBackgroundColor: i === 0 ? 'rgb(74,144,226)' : i === 1 ? 'rgb(39, 189, 156)' : 'rgb(227, 80, 80)',
           borderWidth : 2,
+          tension: 0.3,
+          pointStyle : 'circle',
+          pointBackgroundColor: 'white',
+          pointHoverBorderColor: 'white',
+          pointHoverBorderWidth: 3,
+          radius: 5,
+          pointHoverRadius: 8,
         })
       }
-    }
-    // console.log(labels);
-    // console.log(datatset);
-    setData({ labels, datatset });
+      setData({ labels, datatset });
+    }   // console.log(labels); console.log(datatset);
   },[chart]);
-
   console.log(data);
 
   return (
@@ -58,7 +89,7 @@ const ChartSection = () => {
         <div className='flex gap-4'>
           <div className='flex-4'>rank</div>
           <div className='flex-6'>
-            { data && <Line data={ data } options={ options } /> }
+            {/* { data && <Line  data={ data } options={ options } /> ERROR } */}
           </div>
         </div>
       </div>
